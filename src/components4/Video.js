a2acaccd706b9fa4daba0ae2fe989c03c38a1a41
OSO <nicholas.molina.t@gmail.com>
2022-06-30 01:04:59 -0400

    cambios ultimos

--- src/components4/Video.js
+++ src/components4/Video.js
@@ -1,23 +1,27 @@
 import React from 'react'
 import ReactPlayer from 'react-player'
+import "./styles/video.css"
 
 const Video = () => {
   return (
     <div className=' '>
         <h2 className='flex justify-center text-[#f6d3ff] text-lg'>NOTICIAS</h2>
-        <div className='sm:flex w-full items-center justify-around mx-auto'>
+        
+          <div className='m-1 justify-center items-center flex '>
+              <div className='videoC w-full'>
+                      <ReactPlayer url='https://www.youtube.com/watch?v=ju3NphjdNfs'
+                      controls 
+                      playing
+                      muted
+                      loop
+                      width="100%"
+                      height="100%"
+                      />
+                </div>
+            </div>
+            
+        <div className='flex sm:flex w-full items-center justify-around mx-auto'>
           <div className=' flex flex-wrap justify-center '>
-              <div className='m-1 flex  '>
-                  <ReactPlayer url='https://www.youtube.com/watch?v=ju3NphjdNfs'
-                  controls 
-                  playing
-                  muted
-                  loop
-                  width="100%"
-                  height="100%"
-                  />
-              </div>
-
               <div className='  m-1 sm:item-center'>
                 <iframe src="https://www.instagram.com/p/Ccy-BHFreDT/embed" 
                 className='flex'
